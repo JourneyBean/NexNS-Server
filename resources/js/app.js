4ba629a5878e392b6fba28103dfec3a12059ecd7
Johnson Liu <johnson@awaworks.net>
2022-06-26 22:24:48 +0800

    Init: Setup Inertiajs and Vuejs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
 import './bootstrap';
+
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/inertia-vue3';
+
+const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'NexNS';
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: name => require(`./Pages/${name}`),
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .mount(el)
+    },
+});
